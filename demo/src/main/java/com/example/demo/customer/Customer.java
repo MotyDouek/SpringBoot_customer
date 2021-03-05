443d39840701ac4a707ce5f71885823370fab47f
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-03-05 21:09:05 +0200

    adding email parm

--- demo/src/main/java/com/example/demo/customer/Customer.java
+++ demo/src/main/java/com/example/demo/customer/Customer.java
@@ -3,6 +3,7 @@ package com.example.demo.customer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class Customer {
@@ -13,11 +14,15 @@ public class Customer {
     @NotBlank
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private final String password;
+    @NotBlank
+    @Email()
+    private final String email;
 
-    public Customer(Long id, String name, String password) {
+    public Customer(Long id, String name, String password, String email) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.email = email;
     }
 
     @JsonProperty("customerId")
@@ -32,12 +37,17 @@ public class Customer {
     @JsonIgnore
     public String getPassword() { return password; }
 
+    public String getEmail() {
+        return email;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
