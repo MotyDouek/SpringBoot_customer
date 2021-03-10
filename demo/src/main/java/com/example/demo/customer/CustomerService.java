1ddedc68e1c38cc842befa1870a8fda4fe1f0385
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-03-10 22:17:25 +0200

    NotFound exception

--- demo/src/main/java/com/example/demo/customer/CustomerService.java
+++ demo/src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.demo.customer;
 
+import com.example.demo.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,7 @@ public class CustomerService {
     Customer getCustomer(Long id) {
         return getCustomer().stream()
                 .filter(customer -> customer.getId().equals(id))
-                .findFirst().orElseThrow(() -> new IllegalStateException("customer with id " + id + " not found"));
+                .findFirst().orElseThrow(() -> new NotFoundException("customer with id " + id + " not found"));
     }
 
 }
